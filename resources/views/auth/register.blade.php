e8857409d7a1a41115b257b5b383d912b3110acd
10969torrexx <83467428+10969torrexx@users.noreply.github.com>
2024-05-13 21:43:44 +0800

    enable login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -137,50 +137,48 @@
               <!-- /Logo -->
               <h4 class="mb-2">Adventure starts here 🚀</h4>
               <p class="mb-4">Make your app management easy and fun!</p>
-
-              <form id="formAuthentication" class="mb-3" action="index.html" method="POST">
-                <div class="mb-3">
-                  <label for="username" class="form-label">Username</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="username"
-                    name="username"
-                    placeholder="Enter your username"
-                    autofocus
-                  />
-                </div>
-                <div class="mb-3">
-                  <label for="email" class="form-label">Email</label>
-                  <input type="text" class="form-control" id="email" name="email" placeholder="Enter your email" />
-                </div>
-                <div class="mb-3 form-password-toggle">
-                  <label class="form-label" for="password">Password</label>
-                  <div class="input-group input-group-merge">
+                <form id="formAuthentication" class="mb-3" action="index.html" method="POST">
+                    <div class="mb-3">
+                    <label for="username" class="form-label">Username</label>
                     <input
-                      type="password"
-                      id="password"
-                      class="form-control"
-                      name="password"
-                      placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
-                      aria-describedby="password"
+                        type="text"
+                        class="form-control"
+                        id="username"
+                        name="username"
+                        placeholder="Enter your username"
+                        autofocus
                     />
-                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
-                  </div>
-                </div>
+                    </div>
+                    <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="text" class="form-control" id="email" name="email" placeholder="Enter your email" />
+                    </div>
+                    <div class="mb-3 form-password-toggle">
+                    <label class="form-label" for="password">Password</label>
+                    <div class="input-group input-group-merge">
+                        <input
+                        type="password"
+                        id="password"
+                        class="form-control"
+                        name="password"
+                        placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                        aria-describedby="password"
+                        />
+                        <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                    </div>
+                    </div>
 
-                <div class="mb-3">
-                  <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="terms-conditions" name="terms" />
-                    <label class="form-check-label" for="terms-conditions">
-                      I agree to
-                      <a href="javascript:void(0);">privacy policy & terms</a>
-                    </label>
-                  </div>
-                </div>
-                <button class="btn btn-primary d-grid w-100">Sign up</button>
-              </form>
-
+                    <div class="mb-3">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="terms-conditions" name="terms" />
+                        <label class="form-check-label" for="terms-conditions">
+                        I agree to
+                        <a href="javascript:void(0);">privacy policy & terms</a>
+                        </label>
+                    </div>
+                    </div>
+                    <button class="btn btn-primary d-grid w-100">Sign up</button>
+                </form>
               <p class="text-center">
                 <span>Already have an account?</span>
                 <a href="/">
@@ -216,5 +214,47 @@
 
     <!-- Place this tag in your head or just before your close body tag. -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
+
+    
+    <script>
+        function decodeJwtResponse(token){
+        let base64url = token.split('.')[1];
+        let base64 = base64url.replace(/-/g, '+').replace(/_/g, '/');
+        let jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) { 
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+        return JSON.parse(jsonPayload);
+        }
+    
+        window.onSignIn = googleUser =>{
+        var user = decodeJwtResponse(googleUser.credential);
+            if(user){
+                $.ajaxSetup({
+                    headers: {  'X-CSRF-TOKEN' : $('meta[name="csrf-token"]').attr('content') }
+                });
+                $.ajax({
+                url: `{{ route('loginGoogle') }}`,
+                method: 'POST',
+                data: {
+                    email: user.email,
+                    name: user.name
+                },
+                beforeSend: function(){
+                    $('#btnLogin').html("REDIRECTING...").prop("disabled", true);
+                },
+                    success:function(response){
+                        console.log(response);
+                        if (response.status == 400 || response.status == 200) {
+                            $('#btnLogin').html("Login").prop("disabled", false);
+                            window.location.href ="/home";
+                        }
+                    },
+                    error:function(xhr, status, error){
+                        alert(xhr.responseJSON.message);
+                    }
+                });
+            }
+        }
+   </script>
   </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GoogleSigninController;
 
 Route::get('/', function () {
     return view('auth.login');
@@ -13,3 +14,6 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/index', function() {
     return view('template.cards-basic');
 });
+
+# handle google
+Route::post('auth/google', [GoogleSigninController::class, 'store'])->name('googleSignIn');
