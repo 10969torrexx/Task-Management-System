d03ddb36d3848921dfc92f5ce87ece61bd27d6fb
10969torrexx <83467428+10969torrexx@users.noreply.github.com>
2024-05-14 01:31:39 +0800

    include tasks

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GoogleSigninController;
+use App\Http\Controllers\TasksController;
 
 Route::get('/', function () {
     return view('auth.login');
@@ -17,3 +18,9 @@ Route::get('/index', function() {
 
 # handle google
 Route::post('auth/google', [GoogleSigninController::class, 'store'])->name('googleSignIn');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/tasks', function() {
+        return 'tasks';
+    });
+});
