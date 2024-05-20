42314849a61586fdab8d2a118d42300f57b9ec67
10969torrexx <83467428+10969torrexx@users.noreply.github.com>
2024-05-20 22:48:25 +0800

    disable creating account from login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -223,8 +223,13 @@
                             });
                         }
                         if (response.status == 300) {
-                            console.log(response.account.email);
-                            window.location.href = `/emailotp/${response.account.email}`;
+                            Swal.fire({
+                                title: 'Error!',
+                                text: response.message,
+                                icon: 'error',
+                                confirmButtonText: 'OK'
+                            });
+                            $('#btnLogin').html("Login").prop("disabled", false);
                         }
                     },
                     error:function(xhr, status, error){
